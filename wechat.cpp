879550a0f9eeb86f096719836bc7d0f430728c08
SJ Chen <sjchen1994@foxmail.com>
2017-09-12 18:00:15 +0800

    9.12

--- wechat.cpp
+++ wechat.cpp
@@ -29,6 +29,7 @@ void wechat::on_wechat_login_released()
 {
     QString url = "https://login.weixin.qq.com/jslogin?appid=wx782c26e4c19acffb&redirect_uri=https%3A%2F%2Fwx.qq.com%2Fcgi-bin%2Fmmwebwx-bin%2Fwebwxnewloginpage&fun=new&lang=en_US&_=1453725386008";
     QString test_url = "http://api.fanyi.baidu.com/api/trans/vip/translate?q=1&from=zh&to=en&appid=20170809000071446&salt=41&sign=9ef6559e825dd2551bed729478562239";
+    QString test1_url = "https://login.wx.qq.com/jslogin?appid=wx782c26e4c19acffb&redirect_uri=https%3A%2F%2Flogin.weixin.qq.com%2Fcgi-bin%2Fmmwebwx-bin%2Fwebwxnewloginpage&fun=new&lang=zh_CN&_=1505208345578";
     we_manager->get(QNetworkRequest(url));
     //QByteArray postdata;
     //QNetworkReply* reply = we_manager->post(QNetworkRequest(url), postdata);
